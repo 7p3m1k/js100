714040a56524f81719d3fe8a253d829349e65985
7p3m1k <kor0885@gmail.com>
2021-05-28 14:16:12 +0900

    변수명 수정

--- 29.js
+++ 29.js
@@ -2,9 +2,8 @@
 
 // 알파벳 하나만을 입력하고 그 알파벳이 대문자이면 YES를 아니면 NO를 출력하는 프로그램을 만들어 주세요.
 
+const checkToLowercase = str => {
+  str === str.toUpperCase() ? "yes" : "no";
+};
 
-const check = (str) => {
-  return str === str.toUpperCase() ? "yes" : "no"}
-
-
-check("G")
+checkToLowercase("G");
