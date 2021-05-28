9f5314eac8e9d432ad57761aad5984fcf96feba3
7p3m1k <kor0885@gmail.com>
2021-05-28 14:08:34 +0900

    변수명 변경

--- 26.js
+++ 26.js
@@ -3,20 +3,19 @@
 
 // **행성의 한글 이름을 입력하면 영어 이름을 반환하는 프로그램**을 만들어 주세요.
 
-
 const planets = {
-	'수성' : 'Mercury',
-	'금성' : 'Venus',
-	'지구' : 'Earth',
-	'화성' : 'Mars',
-	'목성' : 'Jupiter',
-	'토성' : 'Saturn',
-	'천왕성' : 'Uranus',
-	'해왕성' : 'Neptune',
+  수성: "Mercury",
+  금성: "Venus",
+  지구: "Earth",
+  화성: "Mars",
+  목성: "Jupiter",
+  토성: "Saturn",
+  천왕성: "Uranus",
+  해왕성: "Neptune",
 };
 
-const changePlanetName = (str) => {
-  return planets[str]
-}
+const changePlanetNameKRtoENG = str => {
+  return planets[str];
+};
 
-console.log(changePlanetName("토성"))
+console.log(changePlanetNameKRtoENG("토성"));
